e92ba59fa1195b7360d77632040dfe5cb9d9ecdc
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-27 19:28:10 +0330

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main(){
-	 
-}
+import (
+	"fmt"
+
+	"github.com/pooulad/csv-convertor/config"
+	"github.com/pooulad/csv-convertor/internal/readflag"
+	"github.com/pooulad/csv-convertor/utils"
+)
+
+func main() {
+	flags, err := readflag.ReadFlag()
+	if err != nil {
+		utils.Colorize(utils.ColorRed, err.Error())
+	}
+	fmt.Println(flags)
