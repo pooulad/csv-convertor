30c4ce6cc1173d6c58a124e1e64f5a858c8ac349
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-27 17:16:30 +0330

    Create dbConfig.go

---
+++ config/dbConfig.go
@@ -0,0 +1,16 @@
+package config
+
+type MysqlConfig struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Host string `json:"host"`
+	Name   string `json:"name"`
+}
+
+type PostgresConfig struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Host     string `json:"host"`
+	Name     string `json:"name"`
+	SSL      string `json:"ssl"`
+}
