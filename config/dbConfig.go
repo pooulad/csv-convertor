5d3e64a7be948492e4f3c6efde1b2d7f05e4808c
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-27 17:34:59 +0330

    create ReadMysqlConfig

--- config/dbConfig.go
+++ config/dbConfig.go
@@ -1,10 +1,14 @@
 package config
 
+	"fmt"
+	"os"
+)
+
 type MysqlConfig struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
-	Host string `json:"host"`
-	Name   string `json:"name"`
+	Host     string `json:"host"`
+	Name     string `json:"name"`
 }
 
 type PostgresConfig struct {
@@ -14,3 +18,23 @@ type PostgresConfig struct {
 	Name     string `json:"name"`
 	SSL      string `json:"ssl"`
 }
+
+func ReadMysqlConfig(path string) (*MysqlConfig, error) {
+	if path == "" {
+		return nil, fmt.Errorf("no path provided")
+	}
+
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg MysqlConfig
+	err = json.Unmarshal(file, &cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
+
