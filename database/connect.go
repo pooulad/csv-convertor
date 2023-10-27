3005b4d0620f08ad898f2e6a8219390cc83f9d14
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-28 01:28:13 +0330

    Update connect.go

--- database/connect.go
+++ database/connect.go
@@ -3,29 +3,38 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/lib/pq"
+
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 	"github.com/pooulad/csv-convertor/config"
-	"github.com/pooulad/csv-convertor/internal/readflag"
 )
 
-func ConnectMysql(cfg *config.MysqlConfig, f *readflag.FlagReturns) (*sql.DB, error) {
+func ConnectMysql(cfg *config.MysqlConfig) (*sql.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.Name)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Println(dsn)
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 	return db, nil
 }
 
-func ConnectPostgres(cfg *config.PostgresConfig, f *readflag.FlagReturns) (*sql.DB, error) {
+func ConnectPostgres(cfg *config.PostgresConfig) (*sql.DB, error) {
 	dsn := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s", cfg.Username, cfg.Password, cfg.Host, cfg.Name, cfg.SSL)
+
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
+		fmt.Println("postgres connected")
 		return nil, err
 	}
-	fmt.Println("postgres connected")
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 	return db, nil
 }
